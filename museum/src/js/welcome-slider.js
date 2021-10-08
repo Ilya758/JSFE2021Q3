e22e808394d98bdd99e85323557fb06bfa0a7c3f
Ilya758 <subscriber6436@gmail.com>
2021-10-08 11:10:15 +0300

    feat: added slider to section .welcome

---
+++ museum/src/js/welcome-slider.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    /* eslint-disable no-undef*/
+    new Swiper('.welcome-slider', {
+        navigation: {
+            nextEl: '.welcome-slider__btn-next',
+            prevEl: '.welcome-slider__btn-prev'
+        },
+        pagination: {
+            el: '.welcome-slider__dots',
+            renderBullet(index, className) {
+                return `<li class="dot ${className}" data-count="${++index}"></li>`;
+            },
+            clickable: true,
+        },
+        loop: true,
+    });
+    /* eslint-enable no-undef*/
+    const counter = document.querySelector('.text_state_changable');
+    const welcomeWrapper = document.querySelector('.welcome-slider__wrapper');
+
+    welcomeWrapper.addEventListener('transitionend', () => {
+        const li = document.querySelector('.dot.swiper-pagination-bullet.swiper-pagination-bullet-active');
+        counter.textContent = `0${li.dataset.count}`;
+    });
+});
