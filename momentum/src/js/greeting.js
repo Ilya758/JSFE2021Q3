f820cf39a691282b5285bb512a8d5493ca2e80d9
Ilya758 <subscriber6436@gmail.com>
2021-10-21 20:57:34 +0300

    fix: remove unused function call in greeting.js

--- momentum/src/js/greeting.js
+++ momentum/src/js/greeting.js
@@ -1,15 +1,15 @@
 export default () => {
-    function getGreeting() {
-        const GREETINGS = [
-            'night',
-            'morning',
-            'afternoon',
-            'evening'
-        ];
-        const greetingElement = document.querySelector('.main__greeting');
-        const hours = new Date().getHours();
+    const GREETINGS = [
+        'night',
+        'morning',
+        'afternoon',
+        'evening'
+    ];
+    const greetingElement = document.querySelector('.main__greeting');
+    const hours = new Date().getHours();
 
-        greetingElement.textContent = `Good ${GREETINGS[Math.floor(hours / 6)]},`;
-    }
-    getGreeting();
+    const dayOfTime = GREETINGS[Math.floor(hours / 6)];
+    greetingElement.textContent = `Good ${dayOfTime},`;
+
+    return dayOfTime;
 };
