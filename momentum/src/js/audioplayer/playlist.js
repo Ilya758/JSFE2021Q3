576c1e663658e39082fef3274a40a5e048dff4f4
Ilya758 <subscriber6436@gmail.com>
2021-10-23 23:38:06 +0300

    feat: added playlist.js

---
+++ momentum/src/js/audioplayer/playlist.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        title: '30 Seconds To Mars - Edge of the Earth',
+        src: '../../assets/sounds/30 Seconds To Mars - Edge of the Earth.mp3',
+        duration: '04:38'
+    },
+    {
+        title: 'Black Tide - Shout',
+        src: '../../assets/sounds/Black Tide - Shout.mp3',
+        duration: '03:25'
+    },
+    {
+        title: 'Cardigans - My Favourite Game',
+        src: '../../assets/sounds/Cardigans - My Favourite Game.mp3',
+        duration: '03:36'
+    },
+    {
+        title: 'Fytch - In These Shadows (feat. Carmen Forbes)',
+        src: '../../assets/sounds/Fytch - In These Shadows (feat. Carmen Forbes).mp3',
+        duration: '05:29'
+    },
+    {
+        title: 'Halsey - Castle',
+        src: '../../assets/sounds/Halsey - Castle.mp3',
+        duration: '04:38'
+    },
+    {
+        title: 'Joan Jett And The Blackhearts - I Love Rock N Roll',
+        src: '../../assets/sounds/Joan Jett And The Blackhearts - I Love Rock N Roll.mp3',
+        duration: '02:57'
+    },
+    {
+        title: 'Nick Cave & The Bad Seeds - O Children',
+        src: '../../assets/sounds/Nick Cave & The Bad Seeds - O Children.mp3',
+        duration: '06:51'
+    },
+    {
+        title: 'Saliva - Superstar',
+        src: '../../assets/sounds/Saliva - Superstar.mp3',
+        duration: '04:09'
+    },
+    {
+        title: 'Tom Walker - Leave a Light On',
+        src: '../../assets/sounds/Tom Walker - Leave a Light On.mp3',
+        duration: '03:06'
+    },
+    {
+        title: 'Zedd (ft. Kesha) - True Colors',
+        src: '../../assets/sounds/Zedd (ft. Kesha) - True Colors.mp3',
+        duration: '03:26'
+    }
+];
