8cbd235100b43bdb18d577e7874f84ff24be5edf
Ilya758 <subscriber6436@gmail.com>
2021-10-25 11:15:40 +0300

    feat: implemented async function of receiving data from another photo stocks

---
+++ momentum/src/js/photosource.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-undef */
+export default async (src) => {
+    let preUrl, postUrl, query, key, url, response, data;
+
+    if (src === 'unsplash') {
+        preUrl = 'https://api.unsplash.com/photos/random?orientation=landscape&query=';
+        query = 'evening';
+        key = '&client_id=NhzepGPSFB4OD5WD7qRNoCm1HM0cg-yqkP9h54wRtXk';
+        url = `${preUrl}${query}${key}`;
+        response = await fetch(url);
+        data = await response.json();
+        const img = createImage(data, 'unsplash');
+
+        img.onload = () => {
+            document.body.style.background = `url(${data.urls.full}) 0/cover`;
+        };
+    } else {
+        preUrl = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=';
+        query = 'evening';
+        key = '6bf9d377824659884ff77ad110d2057e';
+        postUrl = '&extras=url_l&format=json&nojsoncallback=1';
+        url = `${preUrl}${key}&tags=${query}${postUrl}`;
+        response = await fetch(url);
+        data = await response.json();
+        const img = createImage(data);
+        img.onload = () => {
+            document.body.style.background = `url(${img.src}) 0/cover`;
+        };
+    }
+
+    function createImage(url, flag) {
+        const img = new Image();
+
+        if (flag === 'unsplash') {
+            img.src = url.urls.full;
+        } else {
+            img.src = url.photos.photo[Math.floor(Math.random() * 100)].url_l;
+        }
+
+        return img;
+    }
+};
