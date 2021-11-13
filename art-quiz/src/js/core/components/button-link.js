bec27bc5300827ebf27a72119c4bf86ac938a825
Ilya758 <subscriber6436@gmail.com>
2021-11-13 18:52:21 +0300

    fix: fixed ButtonLink class

--- art-quiz/src/js/core/components/button-link.js
+++ art-quiz/src/js/core/components/button-link.js
@@ -1,10 +1,14 @@
 import Component from '../templates/component';
 
 class ButtonLink extends Component {
-    constructor(className, id, text) {
+    constructor(className, id, text = '', role) {
         super('a', className);
         this.element.href = `#${id}`;
         this.element.textContent = text;
+
+        if (role) {
+            this.element.dataset.role = role;
+        }
     }
 
     render() {
