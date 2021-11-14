a8af430af1f29cee0c653710f7d2741a2d6bcecc
Ilya758 <subscriber6436@gmail.com>
2021-11-14 23:23:17 +0300

    feat: implemented "Controller" class

---
+++ art-quiz/src/js/app/controller.js
@@ -0,0 +1,20 @@
+import Model from './model';
+import View from './view';
+import Router from './router';
+import Settings from '../pages/settings';
+
+class Controller {
+    constructor() {
+        this.model = new Model();
+        this.view = new View();
+        this.router = new Router();
+    }
+
+    start() {
+        this.view.render(Settings, 'settings');
+        this.view.fillTheVolumeGradient();
+        this.view.initListeners();
+    }
+}
+
+export default Controller;
