651f16a62e2c0d7d65cfb1aed44112910599a41b
Ilya758 <subscriber6436@gmail.com>
2021-11-17 22:13:34 +0300

    feat: added game-state properties for "Model" class

--- art-quiz/src/js/app/model.js
+++ art-quiz/src/js/app/model.js
@@ -1,4 +1,12 @@
 class Model {
+    constructor() {
+        this.gameIsOver = true;
+        this.currentRound = 0;
+        this.roundIsOver = true;
+        this.currentAnswer = false;
+        this.questionInfo = null;
+    }
+
     static commit(item, value) {
         window.localStorage.setItem(item, value);
     }
