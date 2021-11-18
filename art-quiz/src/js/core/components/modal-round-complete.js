824891d1d4279d3c0d2525a842288bbadd1f7d71
Ilya758 <subscriber6436@gmail.com>
2021-11-18 21:44:51 +0300

    feat: implemented "ModalRoundComplete" component with stylization

---
+++ art-quiz/src/js/core/components/modal-round-complete.js
@@ -0,0 +1,63 @@
+import Modal from '../templates/modal';
+import Text from '../components/text';
+import Component from '../templates/component';
+import Button from '../components/button';
+import Card from './card';
+import ButtonLink from './button-link';
+
+class ModalRoundComplete extends Modal {
+    static CLASS = 'modal-complete';
+
+    constructor() {
+        super(ModalRoundComplete.CLASS);
+
+        this.content = this.wrapper.querySelector(
+            `.${ModalRoundComplete.CLASS}__content`
+        );
+
+        this.imgCup = new Component('span', 'icon icon_cup').render();
+        this.heading = new Text(
+            'h2',
+            'text text_color_dark modal-complete__heading',
+            'Congratulations!'
+        ).render();
+        this.currentScoreText = ModalRoundComplete.getCurrentScore();
+
+        this.buttonsContainer = new Component(
+            'div',
+            'modal-complete__buttons-container'
+        ).render();
+        this.homeButton = new ButtonLink(
+            'text text_color_dark modal-complete__button button',
+            'main-page',
+            'Home'
+        ).render();
+        this.nextQuizButton = new Button(
+            'text text_color_dark modal-complete__button button',
+            'button',
+            'next-quiz'
+        ).render();
+        this.nextQuizButton.textContent = 'Next Quiz';
+
+        this.buttonsContainer.append(this.homeButton, this.nextQuizButton);
+
+        this.content.append(
+            this.imgCup,
+            this.heading,
+            this.currentScoreText,
+            this.buttonsContainer
+        );
+    }
+
+    static getCurrentScore() {
+        const score = new Text(
+            'h3',
+            'text text_color_dark modal-complete__score',
+            '8/10'
+        ).render();
+
+        return score;
+    }
+}
+
+export default ModalRoundComplete;
