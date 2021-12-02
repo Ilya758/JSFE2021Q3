df9d0cf06c9b6ada48161d1d6c02b8d78df56965
Ilya758 <subscriber6436@gmail.com>
2021-12-02 13:54:40 +0300

    feat: added declaration file loader-dec.d.ts

---
+++ migration-newip-to-ts/src/components/controller/loader-dec.d.ts
@@ -0,0 +1,40 @@
+declare class Loader {
+    constructor(public baseLink: string, public options: TUrlOptions);
+
+    getResp({ endpoint, options = {} }: IResp): void;
+
+    makeUrl(options: Partial<TRespOptions>, endpoint): TEndpoint;
+
+    errorHandler(res: Response): Response | never;
+
+    load(
+        method: string,
+        endpoint: TEndpoint,
+        callback: (data: Partial<INewsJSON>) => void,
+        options: Partial<TRespOptions>
+    ): void;
+}
+
+type TRespOptions = {
+    sources: TSources;
+};
+
+type TEndpoint = string;
+
+type TSources<T> = {
+    [prop: string]: T;
+};
+
+type TUrlOptions = {
+    [prop: string]: string;
+};
+
+interface IResp {
+    endpoint: TEndpoint;
+    options: Partial<TRespOptions>;
+}
+
+interface INewsJSON {
+    sources: Partial<TSources>;
+    status: string;
+}
