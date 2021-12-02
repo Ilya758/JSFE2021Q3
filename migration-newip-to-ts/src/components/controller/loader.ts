7203960e44a6513d8acfafd37dfadf46069bf61f
Ilya758 <subscriber6436@gmail.com>
2021-12-02 13:53:57 +0300

    fix: fixed function and arguments in loader.ts

--- migration-newip-to-ts/src/components/controller/loader.ts
+++ migration-newip-to-ts/src/components/controller/loader.ts
@@ -1,19 +1,20 @@
 class Loader {
-    constructor(baseLink, options) {
+    constructor(public baseLink: string, public options: TUrlOptions) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
+        { endpoint, options = {} }: IResp,
         callback = () => {
             console.error('No callback for GET response');
         }
-    ) {
+    ): void {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: Response): Response | never {
+        console.log(res);
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,22 +24,29 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
-        const urlOptions = { ...this.options, ...options };
+    makeUrl(options: Partial<TRespOptions>, endpoint: TEndpoint) {
+        const urlOptions: TUrlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
         Object.keys(urlOptions).forEach((key) => {
+            console.log(urlOptions);
             url += `${key}=${urlOptions[key]}&`;
         });
 
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
-        fetch(this.makeUrl(options, endpoint), { method })
-            .then(this.errorHandler)
-            .then((res) => res.json())
-            .then((data) => callback(data))
+    load(
+        method: string,
+        endpoint: string,
+        callback: (data?: Partial<INewsJSON>) => void,
+        options: Partial<TRespOptions> = {}
+    ) {
+        const response = fetch(this.makeUrl(options, endpoint), { method });
+        response
+            .then(this.errorHandler.bind(Loader))
+            .then((res: Response) => res.json())
+            .then((data: Partial<INewsJSON>) => callback(data))
             .catch((err) => console.error(err));
     }
 }
