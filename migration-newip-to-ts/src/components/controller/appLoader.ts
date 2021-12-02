0bc5a9852a7bd5558d52d2fe3b8b1904fdfa1b56
Ilya758 <subscriber6436@gmail.com>
2021-12-02 15:22:53 +0300

    feat: added api link into AppLoader class

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '80af9475067f4ed084cd3dea8f749852',
         });
     }
 }
