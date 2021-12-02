ed0806c832b37a9ed61a22513da3d8fa1cb0ca41
Ilya758 <subscriber6436@gmail.com>
2021-12-02 17:08:20 +0300

    fix: fixed function arguments, functions in AppController class

--- migration-newip-to-ts/src/components/controller/controller.ts
+++ migration-newip-to-ts/src/components/controller/controller.ts
@@ -1,7 +1,7 @@
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: () => void) {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,13 +10,15 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: Event, callback: () => void) {
+        const target = e.target as HTMLElement;
+        const newsContainer = e.currentTarget as HTMLDivElement;
+        let parentNode: ParentNode | null = null;
 
-        while (target !== newsContainer) {
+        while (parentNode || target !== newsContainer) {
             if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
+                const sourceId = target.getAttribute('data-source-id') || '';
+
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
@@ -31,7 +33,8 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+
+            parentNode = target.parentNode;
         }
     }
 }
