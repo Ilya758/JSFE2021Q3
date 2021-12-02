7faa8c7824b908cf27cdea8cf24241323b78d93b
Ilya758 <subscriber6436@gmail.com>
2021-12-02 15:21:38 +0300

    fix: fixed function parameters, functions, class constructor App

--- migration-newip-to-ts/src/components/app/app.ts
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -2,16 +2,19 @@ import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
 class App {
-    constructor() {
+    constructor(public controller: AppController, public view: AppView) {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
-    start() {
-        document
-            .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+    start(): void {
+        const sources = document.querySelector('.sources') as HTMLElement;
+
+        sources.addEventListener('click', (e) =>
+            this.controller.getNews(e, (data: INewsJSON) => this.view.drawNews(data))
+        );
+
+        this.controller.getSources((data: INewsJSON) => this.view.drawSources(data));
     }
 }
 
