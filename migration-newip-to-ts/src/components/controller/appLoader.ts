b9def6200c108503319a90a69e813ed7df31c5f0
Ilya758 <subscriber6436@gmail.com>
2021-12-04 17:41:24 +0300

    fix: fixed pre-link in AppLoader class

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '80af9475067f4ed084cd3dea8f749852',
         });
     }
