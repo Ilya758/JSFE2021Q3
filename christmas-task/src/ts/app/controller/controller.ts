d25c44e8677c044a03ecc4f4c8f1e7d55b48f9a1
Ilya758 <subscriber6436@gmail.com>
2021-12-09 19:41:13 +0300

    feat: added class Controller

---
+++ christmas-task/src/ts/app/controller/controller.ts
@@ -0,0 +1,23 @@
+import Model from '../model/model';
+import Router from '../router/router';
+import View from '../view/view';
+
+class Controller {
+  protected model: Model;
+
+  protected view: View;
+
+  protected router: Router;
+
+  constructor() {
+    this.model = new Model();
+    this.view = new View();
+    this.router = new Router();
+  }
+
+  start(): void {
+    View.render();
+  }
+}
+
+export default Controller;
