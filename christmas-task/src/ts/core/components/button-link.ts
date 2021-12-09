7f19e6682bd9990963a06b383521cebb47c33e4a
Ilya758 <subscriber6436@gmail.com>
2021-12-09 22:06:05 +0300

    feat: implemented component ButtonLink

---
+++ christmas-task/src/ts/core/components/button-link.ts
@@ -0,0 +1,19 @@
+import Component from '../templates/component';
+
+class ButtonLink extends Component {
+  constructor(
+    protected className: string,
+    protected external: boolean,
+    protected id: string,
+    protected text: string = ''
+  ) {
+    super('a', className);
+    this.element = this.element as HTMLAnchorElement;
+
+    (this.element as HTMLAnchorElement).href = this.external ? id : `#${id}`;
+    (this.element as HTMLAnchorElement).target = '_blank';
+    this.element.textContent = text;
+  }
+}
+
+export default ButtonLink;
