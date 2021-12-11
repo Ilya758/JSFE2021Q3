a60d930c0b4209275716986d0e0792040587e029
Ilya758 <subscriber6436@gmail.com>
2021-12-11 18:58:20 +0300

    feat: added class Model with ICard interface

--- christmas-task/src/ts/app/model/model.ts
+++ christmas-task/src/ts/app/model/model.ts
@@ -1,3 +1,16 @@
-class Model {}
+import { ICard } from '../../models/card';
+import data from '../../toys-data/toys';
+
+class Model {
+  protected initArrayOfToys: ICard[];
+
+  constructor() {
+    this.initArrayOfToys = data;
+  }
+
+  getInitArrayOfToys() {
+    return this.initArrayOfToys;
+  }
+}
 
 export default Model;
