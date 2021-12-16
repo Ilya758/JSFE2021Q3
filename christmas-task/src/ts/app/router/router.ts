2344b09a917fb081b28b05b47bb79fe157dc7e27
Ilya758 <subscriber6436@gmail.com>
2021-12-16 18:30:24 +0300

    feat: implemented static method resetHashAfterReload in class Router

--- christmas-task/src/ts/app/router/router.ts
+++ christmas-task/src/ts/app/router/router.ts
@@ -9,6 +9,10 @@ class Router {
     this.hash = window.location.hash ? window.location.hash.slice(1) : '';
     return this.hash;
   }
+
+  static resetHashAfterReload() {
+    window.location.hash = '';
+  }
 }
 
 export default Router;
