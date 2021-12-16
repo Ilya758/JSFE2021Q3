4c7845de7b49b966951bf4d7e57d812626d5f657
Ilya758 <subscriber6436@gmail.com>
2021-12-16 18:25:28 +0300

    fix: removed .footer in class ToysPage

--- christmas-task/src/ts/pages/toys.ts
+++ christmas-task/src/ts/pages/toys.ts
@@ -43,32 +43,7 @@ class ToysPage extends Page {
     cardsContainer.append(cardsHeading, cardsList);
 
     mainContent.append(this.settingsContainer, cardsContainer);
-
-    const footerWrapper = new BEMWrapper('footer', 'footer').render();
-    const footerContent = footerWrapper.querySelector(
-      '.footer__content'
-    ) as HTMLDivElement;
-
-    const copyright = new Text(
-      'span',
-      'text footer__text',
-      '2021 \u00A9'
-    ).render();
-    const author = new ButtonLink(
-      'text footer__author',
-      true,
-      'https://github.com/Ilya758',
-      'app developer: illia skaryna'
-    ).render();
-    const logoSchool = new ButtonLink(
-      'icon footer__logo',
-      true,
-      'https://rs.school',
-      ''
-    ).render();
-
-    footerContent.append(author, copyright, logoSchool);
-    this.root.append(mainWrapper, footerWrapper);
+    this.root.prepend(mainWrapper);
   }
 
   createSettingsContainer(): HTMLDivElement {
