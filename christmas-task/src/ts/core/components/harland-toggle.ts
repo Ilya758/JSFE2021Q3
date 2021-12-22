2feb4fca0d3ae090ad541276e66985ddc4403865
Ilya758 <subscriber6436@gmail.com>
2021-12-22 15:12:11 +0300

    feat: implemented component HarlandToggle with stylization

---
+++ christmas-task/src/ts/core/components/harland-toggle.ts
@@ -0,0 +1,23 @@
+import Component from '../templates/component';
+
+class HarlandToggle extends Component {
+  constructor(protected className: string, protected id: string) {
+    super('div', className);
+    this.element = this.create();
+  }
+
+  create() {
+    // create track
+    const track = this.element;
+    track.id = this.id;
+    track.className = `${this.className}__track`;
+    // create thumb
+    const thumb = new Component('div', `${this.className}__thumb`).render();
+
+    track.append(thumb);
+
+    return track;
+  }
+}
+
+export default HarlandToggle;
