6ef8251f9555a872318d6eef4e08a1942e573313
Ilya758 <subscriber6436@gmail.com>
2021-12-24 19:14:01 +0300

    fix: modified abstract class Page

--- christmas-task/src/ts/core/abstract/page.ts
+++ christmas-task/src/ts/core/abstract/page.ts
@@ -1,11 +1,23 @@
 import { ICard } from '../../models/card';
 
+export type TRenderMethod = {
+  initToysArray: ICard[] | [];
+  chosenToys: ICard[];
+  snowIsFalling: boolean;
+  id: string;
+};
+
 abstract class Page {
   constructor(readonly id: string) {
     this.id = id;
   }
 
-  abstract render(initToysArray: ICard[]): void;
+  abstract render({
+    initToysArray,
+    chosenToys,
+    snowIsFalling,
+    id,
+  }: TRenderMethod): void;
 }
 
 export default Page;
